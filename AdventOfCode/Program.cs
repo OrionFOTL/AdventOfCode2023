8952bb940045d9c5af15d0c1bb60de05cf36e46f
OrionFOTL <8158804+OrionFOTL@users.noreply.github.com>
2024-08-21 20:38:29 +0200

    Add project files.

---
+++ AdventOfCode/Program.cs
@@ -0,0 +1,43 @@
+﻿using System.Text.RegularExpressions;
+
+(string Name, int Value)[] numberNames =
+[
+    ("one", 1),
+    ("two", 2),
+    ("three", 3),
+    ("four", 4),
+    ("five", 5),
+    ("six", 6),
+    ("seven", 7),
+    ("eight", 8),
+    ("nine", 9),
+    ("1", 1),
+    ("2", 2),
+    ("3", 3),
+    ("4", 4),
+    ("5", 5),
+    ("6", 6),
+    ("7", 7),
+    ("8", 8),
+    ("9", 9),
+];
+
+var sum = Input.InputString
+    .Split(Environment.NewLine)
+    .Select(GetFirstAndLastDigit)
+    .Select(digits => digits.FirstDigit * 10 + digits.LastDigit)
+    .Sum();
+
+Console.WriteLine(sum);
+
+(int FirstDigit, int LastDigit) GetFirstAndLastDigit(string line)
+{
+    var numbersPositionsAndValues = numberNames
+        .SelectMany(numberName => Regex.Matches(line, numberName.Name)
+            .Select(match => (PositionInLine: match.Index, Value: numberName.Value)))
+        .OrderBy(n => n.PositionInLine);
+
+    return (
+        FirstDigit: numbersPositionsAndValues.First().Value,
+        LastDigit: numbersPositionsAndValues.Last().Value);
+}
---
+++ Day2/Program.cs
@@ -0,0 +1,19 @@
+﻿var invalidGameNumbersSummed = Input.InputString
+    .Split(Environment.NewLine)
+    .Select(line => line.Split(": "))
+    .Select(x => new
+    {
+        GameNumber = int.Parse(x[0].Split(' ').Last()),
+        Cubes = x[1]
+            .Split("; ")
+            .SelectMany(cubes => cubes
+                .Split(", ")
+                .Select(cubeSet => (Amount: int.Parse(cubeSet.Split(' ')[0]), Color: cubeSet.Split(' ')[1]))),
+    })
+    .Where(game => !game.Cubes
+        .Any(cubeSet => cubeSet is { Color: "red", Amount: > 12 }
+                                or { Color: "green", Amount: > 13 }
+                                or { Color: "blue", Amount: > 14 }))
+    .Sum(game => game.GameNumber);
+
+Console.WriteLine(invalidGameNumbersSummed);
